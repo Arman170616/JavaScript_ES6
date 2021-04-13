26c5f2f3cca4d1caf3ab48dbc564a20ede8fadbf
H.Arman <64675035+Arman170616@users.noreply.github.com>
2021-04-13 17:07:08 +0600

    Create arrowFunction.js

---
+++ arrowFunction.js
@@ -0,0 +1,13 @@
+let sayHello = name => {
+    console.log(name);
+}
+sayHello("arman");
+
+let sayHello = (name, age) => {
+    console.log(name, age);
+}
+sayHello("arman", 25);
+
+
+let sayHello = num => num * num;
+console.log(sayHello(25));
